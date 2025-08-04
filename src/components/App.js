45c9e104563c75b7f26b780fbfade62ca68d0918
ajain414 <abhei780@gmail.com>
2025-08-04 22:38:25 +0530

    print the quiestion

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,11 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 const initialState = {
   questions: [],
   status: "loading",
+  index: 0,
 };
 const reducer = (state, action) => {
   switch (action.type) {
@@ -14,13 +16,15 @@ const reducer = (state, action) => {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Unhandled action");
   }
 };
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { questions, status } = state;
+  const { questions, status, index } = state;
   const numQuestions = questions.length;
   useEffect(() => {
     fetch("http://localhost:8000/questions")
@@ -35,7 +39,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
